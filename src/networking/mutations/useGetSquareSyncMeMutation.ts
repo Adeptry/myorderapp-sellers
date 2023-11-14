dc51c39f9e791b5c139cbdbdcbb0b25301f1ca7c
PLJNS <me@pljns.com>
2023-11-13 22:11:22 -0500

    2.5.8 MOA-223 must allow sellers to initiate a manual catalog sync v2

--- src/networking/mutations/useGetSquareSyncMeMutation.ts
+++ src/networking/mutations/useGetSquareSyncMeMutation.ts
@@ -1,14 +1,19 @@
 import { useSessionedApiConfiguration } from "@/utils/useSessionedApiConfiguration";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { MerchantsApi } from "myorderapp-square";
+import { GetCategoriesQueryKey } from "../queries/useGetCategoriesMeQuery";
 
 export const useGetSquareSyncMeMutation = () => {
   const sessionedApiConfiguration = useSessionedApiConfiguration();
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async () => {
       return await new MerchantsApi(
         sessionedApiConfiguration
       ).getSquareSyncMe();
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: GetCategoriesQueryKey });
     },
   });
 };
